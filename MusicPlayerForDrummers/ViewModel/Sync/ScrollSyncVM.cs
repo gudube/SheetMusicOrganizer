288c06873445b31f8f36fd0e6d6e55bf4693e49b
Guilhem <guilhem.dubois@polymtl.ca>
2020-09-27 12:08:19 -0400

    implemented buttons to set start/end time

--- MusicPlayerForDrummers/ViewModel/Sync/ScrollSyncVM.cs
+++ MusicPlayerForDrummers/ViewModel/Sync/ScrollSyncVM.cs
@@ -1,5 +1,7 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using MusicPlayerForDrummers.Model.Items;
+using MusicPlayerForDrummers.ViewModel.Tools;
 
 namespace MusicPlayerForDrummers.ViewModel.Sync
 {
@@ -7,6 +9,8 @@ namespace MusicPlayerForDrummers.ViewModel.Sync
     {
         public ScrollSyncVM(SessionContext session) : base(session)
         {
+            SetStartNowCommand = new DelegateCommand(SetStartNow);
+            SetEndNowCommand = new DelegateCommand(SetEndNow);
         }
 
         protected override void Session_PropertyChanged(object sender, PropertyChangedEventArgs e)
@@ -21,5 +25,31 @@ namespace MusicPlayerForDrummers.ViewModel.Sync
             get => _syncingSong;
             set => SetField(ref _syncingSong, value);
         }
+
+        public DelegateCommand SetStartNowCommand { get; }
+        private void SetStartNow(object? obj)
+        {
+            if (SyncingSong != null)
+            {
+                double timeFromStart = Session.Player.Position;
+                double endFromStart = Session.Player.Length - SyncingSong.ScrollEndTime;
+                if (timeFromStart > endFromStart - 10)
+                    return;
+                SyncingSong.ScrollStartTime = (int) Math.Ceiling(timeFromStart);
+            }
+        }
+
+        public DelegateCommand SetEndNowCommand { get; }
+        private void SetEndNow(object? obj)
+        {
+            if (SyncingSong != null)
+            {
+                double timeFromStart = Session.Player.Position;
+                if (timeFromStart < SyncingSong.ScrollStartTime + 10)
+                    return; //todo: add error somehow
+                SyncingSong.ScrollEndTime = (int) Math.Floor(Session.Player.Length - timeFromStart);
+            }
+        }
+
     }
 }
