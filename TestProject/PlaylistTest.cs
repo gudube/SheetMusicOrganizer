39b2d02059d372ee07bec5b61105df3f3759a777
Guilhem Dubois <gdubois@prettyobjects.com>
2021-09-01 23:07:46 -0400

    Fixed tests! Can now add more tests.

--- TestProject/PlaylistTest.cs
+++ TestProject/PlaylistTest.cs
@@ -1,21 +1,25 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
 using OpenQA.Selenium.Interactions;
-using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace TestProject
 {
+    [TestFixture]
     class PlaylistTest : BaseTestSession
     {
-        [Test]
+        private WindowsElement PlaylistsListBox { get => session.FindElementByClassName("PlaylistsListBox"); }
+        private ReadOnlyCollection<AppiumWebElement> Playlists { get => PlaylistsListBox.FindElementsByName("SheetMusicOrganizer.Model.Items.PlaylistItem"); }
+        private AppiumWebElement AddPlaylistButton { get => PlaylistsListBox.FindElementByName("SheetMusicOrganizer.Model.Items.AddPlaylistItem"); }
+
+
+        [Test, Order(1)]
         public void DefaultPlaylistTest()
         {
-            var playlists = session.FindElementsByClassName("SelectablePlaylistItem");
+            var playlists = Playlists;
             Assert.AreEqual(1, playlists.Count, "There should only be one playlist created by default.");
             var defaultPlaylistTextBlocks = playlists[0].FindElementsByClassName("TextBlock");
             Assert.AreEqual(1, defaultPlaylistTextBlocks.Count, "There should be one text element visible inside a playlist.");
@@ -27,8 +31,8 @@ namespace TestProject
         [Test]
         public void CreatePlaylistTest()
         {
-            var oldPlaylists = session.FindElementsByClassName("SelectablePlaylistItem");
-            var addPlaylistButtons = session.FindElementsByClassName("PlaylistAdderItem");
+            var oldPlaylists = Playlists;
+            var addPlaylistButtons = PlaylistsListBox.FindElementsByName("SheetMusicOrganizer.Model.Items.AddPlaylistItem"); ;
             Assert.AreEqual(1, addPlaylistButtons.Count, "There should only be one 'add playlist' button.");
             Assert.AreEqual(0, addPlaylistButtons[0].FindElementsByClassName("TextBox").Count, "The 'add playlist' button should not be editable before clicking it.");
 
@@ -38,27 +42,27 @@ namespace TestProject
             addPlaylistTextInputs[0].SendKeys(" New Playlist 123!@#<>\\/ ");
             addPlaylistTextInputs[0].SendKeys(Keys.Enter);
 
-            var newPlaylists = session.FindElementsByClassName("SelectablePlaylistItem");
+            var newPlaylists = Playlists;
             Assert.AreEqual(oldPlaylists.Count + 1, newPlaylists.Count, "A new playlist should be created after pressing enter.");
-            Assert.AreEqual(true, newPlaylists.Last().Selected, "The new playlist should be selected.");
-            Assert.AreEqual(" New Playlist 123!@#<>\\/ ", newPlaylists.Last().FindElementByClassName("TextBox").Text, "The new playlist should have the name entered.");
-            addPlaylistButtons = session.FindElementsByClassName("PlaylistAdderItem");
-            Assert.AreEqual(0, addPlaylistButtons[0].FindElementsByClassName("TextBox").Count, "The 'add playlist' button should not be editable after confirming.");
+            // Assert.AreEqual(true, newPlaylists.Last().GetAttribute("SelectionItem.IsSelected"));
+            Assert.AreEqual(true, newPlaylists[1].Selected);
+            Assert.AreEqual(" New Playlist 123!@#<>\\/ ", newPlaylists[1].FindElementByClassName("TextBlock").Text, "The new playlist should have the name entered.");
+            Assert.AreEqual(0, AddPlaylistButton.FindElementsByClassName("TextBox").Count, "The 'add playlist' button should not be editable after confirming.");
         }
 
         [Test]
         public void CancelEscPlaylistTest()
         {
             // test using escape key, clicking on another playlist or clicking on another selectable element (e.g. song or mastery level)
-            var oldPlaylists = session.FindElementsByClassName("SelectablePlaylistItem");
-            var addPlaylistButton = session.FindElementByClassName("PlaylistAdderItem");
+            var oldPlaylists = Playlists;
+            var addPlaylistButton = AddPlaylistButton;
             addPlaylistButton.Click();
             var addPlaylistTextInput = addPlaylistButton.FindElementByClassName("TextBox");
             addPlaylistTextInput.SendKeys("CancelThisPlaylist");
             
             addPlaylistTextInput.SendKeys(Keys.Escape);
 
-            var newPlaylists = session.FindElementsByClassName("SelectablePlaylistItem");
+            var newPlaylists = Playlists;
             Assert.AreEqual(oldPlaylists.Count, newPlaylists.Count, "No new playlist should be created after pressing Escape.");
             Assert.AreEqual(0, addPlaylistButton.FindElementsByClassName("TextBox").Count, "The 'add playlist' button should not be editable after escaping.");
             Assert.AreEqual(true, newPlaylists.Last().Selected, "The last playlist should be selected after pressing Escape.");
@@ -69,15 +73,15 @@ namespace TestProject
         public void CancelFocusPlaylistTest()
         {
             // test using escape key, clicking on another playlist or clicking on another selectable element (e.g. song or mastery level)
-            var oldPlaylists = session.FindElementsByClassName("SelectablePlaylistItem");
-            var addPlaylistButton = session.FindElementByClassName("PlaylistAdderItem");
+            var oldPlaylists = Playlists;
+            var addPlaylistButton = AddPlaylistButton;
             addPlaylistButton.Click();
             var addPlaylistTextInput = addPlaylistButton.FindElementByClassName("TextBox");
             addPlaylistTextInput.SendKeys("CancelThisPlaylist");
 
             oldPlaylists.First().Click();
 
-            var newPlaylists = session.FindElementsByClassName("SelectablePlaylistItem");
+            var newPlaylists = Playlists;
             Assert.AreEqual(oldPlaylists.Count, newPlaylists.Count, "No new playlist should be created after losing focus.");
             Assert.AreEqual(0, addPlaylistButton.FindElementsByClassName("TextBox").Count, "The 'add playlist' button should not be editable after losing focus.");
             Assert.AreEqual(true, oldPlaylists.First().Selected, "The last playlist should be selected when clicking on it while adding new playlist.");
@@ -86,12 +90,12 @@ namespace TestProject
         [Test]
         public void RenamePlaylistTest()
         {
-            var addPlaylistButton = session.FindElementByClassName("PlaylistAdderItem");
+            var addPlaylistButton = AddPlaylistButton;
             addPlaylistButton.Click();
             var addPlaylistTextInput = addPlaylistButton.FindElementByClassName("TextBox");
             addPlaylistTextInput.SendKeys("Old Playlist");
             addPlaylistTextInput.SendKeys(Keys.Enter);
-            var oldPlaylists = session.FindElementsByClassName("SelectablePlaylistItem");
+            var oldPlaylists = Playlists;
 
             Actions rightClick = new Actions(session);
             rightClick.ContextClick(oldPlaylists.Last()).Perform();
@@ -103,19 +107,20 @@ namespace TestProject
             playlistTextBox.SendKeys("New Playlist");
             playlistTextBox.SendKeys(Keys.Enter);
 
-            Assert.AreEqual(0, oldPlaylists.Last().FindElementsByClassName("TextBox"), "The playlist shouldn't be editable after renaming it.");
-            Assert.AreEqual("New Playlist", oldPlaylists.Last().FindElementByClassName("TextBlock").Text, "The playlist should have the new name after renaming it.");
+            var newPlaylists = Playlists;
+            Assert.AreEqual(0, newPlaylists.Last().FindElementsByClassName("TextBox").Count, "The playlist shouldn't be editable after renaming it.");
+            Assert.AreEqual("New Playlist", newPlaylists.Last().FindElementByClassName("TextBlock").Text, "The playlist should have the new name after renaming it.");
         }
 
         [Test]
         public void DeletePlaylistTest()
         {
-            var addPlaylistButton = session.FindElementByClassName("PlaylistAdderItem");
+            var addPlaylistButton = AddPlaylistButton;
             addPlaylistButton.Click();
             var addPlaylistTextInput = addPlaylistButton.FindElementByClassName("TextBox");
             addPlaylistTextInput.SendKeys("Old Playlist");
             addPlaylistTextInput.SendKeys(Keys.Enter);
-            var oldPlaylists = session.FindElementsByClassName("SelectablePlaylistItem");
+            var oldPlaylists = Playlists;
             var oldPlaylistsCount = oldPlaylists.Count;
 
             Actions rightClick = new Actions(session);
@@ -125,7 +130,7 @@ namespace TestProject
             var deleteMenuItem = contextMenus[0].FindElementByName("Delete");
             deleteMenuItem.Click();
 
-            var newPlaylists = session.FindElementsByClassName("SelectablePlaylistItem");
+            var newPlaylists = Playlists;
             Assert.AreEqual(oldPlaylistsCount - 1, newPlaylists.Count, "There should be one less playlist after deleting one.");
             Assert.AreEqual(true, newPlaylists.Last().Selected, "The playlist on top of the deleted one should be selected.");
         }
