28f6c89358cef9245f4b546f0977eec45f054059
Guilhem Dubois <gdubois@prettyobjects.com>
2022-01-07 12:33:01 -0500

    autoscroll import results

--- SheetMusicOrganizer/View/Windows/ImportResultWindow.xaml.cs
+++ SheetMusicOrganizer/View/Windows/ImportResultWindow.xaml.cs
@@ -22,5 +22,29 @@ namespace SheetMusicOrganizer.View.Windows
         {
             await importTask;
         }
+
+        private bool AutoScroll = true;
+
+        private void Scroller_ScrollChanged(object sender, System.Windows.Controls.ScrollChangedEventArgs e)
+        {
+            // User scroll event : set or unset auto-scroll mode
+            if (e.ExtentHeightChange == 0)
+            {   // Content unchanged : user scroll event
+                if (Scroller.VerticalOffset == Scroller.ScrollableHeight)
+                {   // Scroll bar is in bottom, Set auto-scroll mode
+                    AutoScroll = true;
+                }
+                else
+                {   // Scroll bar isn't in bottom, Unset auto-scroll mode
+                    AutoScroll = false;
+                }
+            }
+
+            // Content scroll event : auto-scroll eventually
+            if (AutoScroll && e.ExtentHeightChange != 0)
+            {   // Content changed and auto-scroll mode set
+                Scroller.ScrollToVerticalOffset(Scroller.ExtentHeight);
+            }
+        }
     }
 }
