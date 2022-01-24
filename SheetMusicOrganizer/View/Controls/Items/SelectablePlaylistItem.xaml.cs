e838efe552d6e778595bbe2e6b4d8bfa4e08da63
Guilhem Dubois <gdubois@prettyobjects.com>
2022-01-24 14:40:46 -0500

    fix lost focus unselects rename playlist

--- SheetMusicOrganizer/View/Controls/Items/SelectablePlaylistItem.xaml.cs
+++ SheetMusicOrganizer/View/Controls/Items/SelectablePlaylistItem.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using SheetMusicOrganizer.Model.Items;
+using SheetMusicOrganizer.ViewModel;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
@@ -20,6 +22,33 @@ namespace SheetMusicOrganizer.View.Controls.Items
             {
                 Keyboard.Focus(MainTextBox);
                 MainTextBox.SelectAll();
+            }
+        }
+
+        private void HideInput()
+        {
+            if (Main.Tag is LibraryVM libraryVM)
+                libraryVM.CancelEditPlaylistCommand?.Execute(null);
+        }
+
+        private void MainTextBox_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            HideInput();
+        }
+
+        private void MainTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            HideInput();
+        }
+
+        private void MainTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (Main.Tag is LibraryVM libraryVM)
+            {
+                if (e.Key == Key.Escape)
+                    HideInput();
+                else if (e.Key == Key.Enter)
+                    libraryVM.RenameSelectedPlaylistCommand?.Execute(null);
             }
         }
     }
